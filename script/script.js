86e89863650692e4b423b05773e79269b29f5bc0
sceletoniK <L.s1n56@yandex.ru>
2023-03-18 15:26:19 +0300

    added pathnames

--- script/script.js
+++ script/script.js
@@ -1,27 +1,29 @@
-var page = "blocks/profile.html"
+
 var main = null
 routes = {}
-routes[(window.location.origin + "/profile")] = "blocks/profile.html"
-routes[(window.location.origin + "/map")] = "blocks/map.html"
-routes[(window.location.origin + "/timer")] = "blocks/timer.html"
+routes["/WeeBee-test/"] = "blocks/profile.html"
+routes["/WeeBee-test/map"] = "blocks/map.html"
+routes["/WeeBee-test/timer"] = "blocks/timer.html"
 
 async function addPage() {
-    const html = await fetch(page).then(res => res.text())
+    
+    const page = window.location.pathname
+    const html = await fetch(routes[page]).then(res => res.text())
     main.innerHTML = html;
     switch(page)
     {
-        case("blocks/profile.html"):
+        case("/WeeBee-test/"):
             if (timePrinter != null) clearInterval(timePrinter)
             timePrinter = null
             links[0].classList.add("active")
             break;
-        case("blocks/map.html"):
+        case("/WeeBee-test/map"):
             if (timePrinter != null) clearInterval(timePrinter)
             timePrinter = null
             links[1].classList.add("active")
             ymaps.ready(init)
             break;  
-        case("blocks/timer.html"):
+        case("/WeeBee-test/timer"):
             if (timePrinter == null) timePrinter = setInterval(printTime, 1000);
             timerP = document.querySelector(".timer_text")
             printTime()
@@ -30,19 +32,22 @@ async function addPage() {
     }
 }
 
-function changePage(value){
+function changePage(){
     links.forEach(item => item.classList.remove("active"))
-    page = routes[value]
     addPage()
 }
 
 function Load(){
+    window.onpopstate = changePage;
     main = document.querySelector('main')
     links = document.querySelectorAll('a')
-    links.forEach(item => item.addEventListener('click', (e) => {
-        
-        changePage(item.href); e.preventDefault();
+    links.forEach(item => 
+        item.addEventListener('click', (e) => {
+        e.stopPropagation();
+        e.preventDefault();
+        window.history.pushState({},'',item.href)
+        changePage(); 
     }))
-    addPage()
+    changePage()
 }
 
